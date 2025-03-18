67350d543747c064d37005bdfb79d6acca356931
getITfromMike <101622604+getitfrommike@users.noreply.github.com>
2025-03-17 23:09:41 -0700

    From a new process
    Adding files outside of the folder.

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('listen-button');
+
+    button.addEventListener('click', function() {
+        window.open('https://www.youtube.com/watch?v=CQ_H4AsFGoE', '_blank');
+    });
+});
