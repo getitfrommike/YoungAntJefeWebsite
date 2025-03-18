5568e5d1bef9cf1a1bc7129c04d0bc578e7fcec9
getITfromMike <101622604+getitfrommike@users.noreply.github.com>
2025-03-17 23:17:20 -0700

    Delete youngantjefe directory
    Need to go directly tto "root" if that makes sense...

--- youngantjefe/scripts.js
+++
@@ -1,9 +0,0 @@
-// scripts.js
-
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('listen-button');
-
-    button.addEventListener('click', function() {
-        window.open('https://www.youtube.com/watch?v=CQ_H4AsFGoE', '_blank');
-    });
-});
